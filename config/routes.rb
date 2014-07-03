0858c4ece68db3f0f679975807ebcfc1384a4eb5
huicheese <huicheese@gmail.com>
2014-07-03 03:57:50 +0000

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 App3::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
