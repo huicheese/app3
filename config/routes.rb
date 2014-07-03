68f5cdeff44cfd04162b4904eeb21355ac9c3ab6
huicheese <huicheese@gmail.com>
2014-07-03 02:58:34 +0000

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 App3::Application.routes.draw do
-  get "users/new"
-
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
